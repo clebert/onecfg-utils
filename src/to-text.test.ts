75704c5631e95fb2c856f4a17acb16684dd21cae
Clemens Akens <clebert@me.com>
2022-01-30 00:44:05 +0100

    Add unit test

--- src/to-text.test.ts
+++ src/to-text.test.ts
@@ -2,6 +2,7 @@ import {toText} from './to-text';
 
 describe(`toText()`, () => {
   test(`generates text`, () => {
+    expect(toText([])).toBe(`\n`);
     expect(toText([`foo`, `bar`, `baz`])).toBe(`foo\nbar\nbaz\n`);
   });
 });
